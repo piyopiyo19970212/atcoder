bad9d1174c585c17c78fee9a5f65d525321bccd0
koharu_okahana <koharu@re2.org>
2020-04-26 18:49:07 +0900

    ac

--- ABC/142/A.cpp
+++ ABC/142/A.cpp
@@ -8,6 +8,6 @@ int main()
     cin >> n;
     if(n==1){ cout << 1 << endl; }
     else if(n%2==0) { cout << 0.5 << endl; }
-    else { cout << 0.6 << endl;}
+    else { cout << (n+1)/(n*2.0)<< endl;}
     return 0;
 }
