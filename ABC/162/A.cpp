aff65f154b3b26a5c672119b803a363ad8a35d01
koharu_okahana <koharu@re2.org>
2020-04-26 16:39:57 +0900

    A.cpp

---
+++ ABC/162/A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main()
+{  
+    string s;
+    cin >> s;
+    for(int i=0; i<s.length(); i++) {
+        if(s[i]=='7'){
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+    return 0;
+}
